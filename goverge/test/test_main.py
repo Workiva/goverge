3159899f9a43d087e9ac26b67e0560df30e56213
wesleybalvanz-wf <wesley.balvanz@workiva.com>
2015-12-07 14:52:38 -0600

    Add tests for main.py

---
+++ goverge/test/test_main.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+from goverge import main
+
+
+class parse_argsTestCase(TestCase):
+    def test_godep(self):
+        args = main._parse_args(['--godep'])
+        expected = {'godep': True, 'short': False, 'test_path': None}
+        self.assertEqual(expected, vars(args))
+
+    def test_short(self):
+        args = main._parse_args(['--short'])
+        expected = {'godep': False, 'short': True, 'test_path': None}
+        self.assertEqual(expected, vars(args))
+
+    def test_path(self):
+        args = main._parse_args([
+            "--test_path=/foo/bar",
+            "--test_path=/bar/foo"
+        ])
+        expected = {
+            'godep': False,
+            'short': False,
+            'test_path': ['/foo/bar', '/bar/foo']
+        }
+        self.assertEquals(expected, vars(args))
+
+class goverge_executeTestCase(TestCase):
+    
