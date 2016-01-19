4c465d8ddd5cc85fd366c80d0dcec5bd26506615
wesleybalvanz-wf <wesley.balvanz@workiva.com>
2016-01-19 16:09:19 -0600

    Don't set the branch name in codecov script

--- codecov.sh
+++ codecov.sh
@@ -5,5 +5,5 @@ then
     echo "GIT_BRANCH environment variable not set, skipping codecov push"
 else
     TRACKING_REMOTE="$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD) | cut -d'/' -f1 | xargs git ls-remote --get-url | cut -d':' -f2 | sed 's/.git$//')"
-    codecov -u https://codecov.io -b $GIT_BRANCH -r $TRACKING_REMOTE --no-fail
+    codecov -u https://codecov.io -r $TRACKING_REMOTE --no-fail
 fi
