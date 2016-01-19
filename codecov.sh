8cd9b9cafd183b30065c4da8126ae56376a85d37
wesleybalvanz-wf <wesley.balvanz@workiva.com>
2016-01-19 16:12:05 -0600

    Remove if block

--- codecov.sh
+++ codecov.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-
-if [ -z "$GIT_BRANCH" ]
-then
-    echo "GIT_BRANCH environment variable not set, skipping codecov push"
-else
-    TRACKING_REMOTE="$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD) | cut -d'/' -f1 | xargs git ls-remote --get-url | cut -d':' -f2 | sed 's/.git$//')"
-    codecov -u https://codecov.io -r $TRACKING_REMOTE --no-fail
-fi
+TRACKING_REMOTE="$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD) | cut -d'/' -f1 | xargs git ls-remote --get-url | cut -d':' -f2 | sed 's/.git$//')"
+codecov -u https://codecov.io -r $TRACKING_REMOTE --no-fail
