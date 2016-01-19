967cc2a609463a3a8f24928fb989336e6f57b55a
wesleybalvanz-wf <wesley.balvanz@workiva.com>
2016-01-19 16:14:47 -0600

    Remove script and run codecov in travis.yaml

--- codecov.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-TRACKING_REMOTE="$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD) | cut -d'/' -f1 | xargs git ls-remote --get-url | cut -d':' -f2 | sed 's/.git$//')"
-codecov -u https://codecov.io -r $TRACKING_REMOTE --no-fail
