d34492c4fa1a83467b487aa258c9828ef8c3fb34
Sean Gerhardt <sean.gerhardt@workiva.com>
2016-12-12 13:03:18 -0600

    Remove unused param in coverage.py

--- goverge/coverage.py
+++ goverge/coverage.py
@@ -32,7 +32,7 @@ def check_failed(return_code):
 
 def generate_coverage(
         packages, project_package, project_root, godep, short, xml, xml_dir,
-        race, tag, max_threads, go_flags, ignore):
+        race, tag, max_threads, go_flags):
     """ Generate the coverage for a list of packages.
 
     :type packages: list
@@ -57,8 +57,6 @@ def generate_coverage(
     :param max_threads: The maximum number of threads for the tests to run on
     :type go_flags: list
     :param go_flags: list of go build flags to use when running tests
-    :type ignore: list
-    :param ignore: list of directories to skip coverage
     """
 
     threads = []
@@ -83,7 +81,6 @@ def generate_coverage(
                 race,
                 tag,
                 go_flags,
-                ignore,
             ))
             t.daemon = True
             t.start()
@@ -95,7 +92,7 @@ def generate_coverage(
 
 def generate_package_coverage(
         test_path, project_package, test_package, project_root, godep, short,
-        xml, xml_dir, race, tag, go_flags, ignore):
+        xml, xml_dir, race, tag, go_flags):
     """ Generates the coverage report for a package.
 
     :type test_path: string
@@ -118,8 +115,6 @@ def generate_package_coverage(
     :param tag: A custom build tag to use when running go test
     :type go_flags: list
     :param go_flags: list of go build flags to use when running tests
-    :type ignore: list
-    :param ignore: list of directories to skip coverage
     """
 
     # Get the dependencies of the package we are testing
